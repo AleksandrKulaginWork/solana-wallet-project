a52a53694e354d09d0e03feef984e1ae7bf3c00e
AleksandrKulaginWork <aleksandr.kulagin.work.email@gmail.com>
2024-07-25 15:52:24 +0300

    update

---
+++ next.config.js
@@ -0,0 +1,14 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: "/solana-wallet-project",
+    output: "export",
+    reactStrictMode: true,
+  };
+  
+  module.exports = nextConfig;
